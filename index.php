d69dbc2314b34f6f4b463f0e8f2784c9672c2c48
marcodaluz <marcodaluz@gmail.com>
2021-02-13 18:13:10 -0300

    removendo arquivos de configuracao

--- index.php
+++ index.php
@@ -2,18 +2,24 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Ieq\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
+	$page = new Page();
+
+	$page->setTpl("index");
+
+    // -- Testes iniciais e de banco
 	//echo "OK";
-	$sql = new Ieq\DB\Sql();
-
-	$results = $sql->select("select * from city");
-
-	echo json_encode($results);
+	//$sql = new Ieq\DB\Sql();
+	//$results = $sql->select("select * from city");
+	//echo json_encode($results);
 
 });
 
---
+++ views-cache/header.32e556621ad0df7b2e9d43756bedd8d1.rtpl.php
@@ -0,0 +1,7 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html?&gt;
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>IEQ Maringa</title>
+	</head>
+	<body>
